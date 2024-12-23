8d821983d1034be41b2b54b909228b2d8d623442
Evgeny <evgeny.kochenyuk@gmail.com>
2024-12-23 23:20:59 +0100

    empty Gtk program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Gdk;
+using Gtk;
+
+class MyWindow : Gtk.Window {
+    public MyWindow() : base("hello, world") {
+    }
+
+    protected override bool OnDeleteEvent(Event e) {
+        Application.Quit();
+        return true;
+    }
+}
+
+class Hello {
+    static void Main() {
+        Application.Init();
+        MyWindow w = new MyWindow();
+        w.ShowAll();
+        Application.Run();
+    }
+}
